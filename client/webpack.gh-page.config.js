d92b50d9061cd7994b5c1f475b70810e2e8c22ed
Alex Choi <achoi@hearst.com>
2024-05-07 09:53:19 -0400

    logo set path

--- client/webpack.gh-page.config.js
+++ client/webpack.gh-page.config.js
@@ -75,11 +75,13 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'assets'
                     },
-                ],
+
+                }],
             },
         ],
     },
