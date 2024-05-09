bc4e5ee1d475d043238bc7464f9b98ebc4589d69
Alex Choi <achoi@hearst.com>
2024-05-09 00:07:42 -0400

    add json middleware

--- server/index.js
+++ server/index.js
@@ -6,16 +6,21 @@ const router = require('./routes');
 const app = express();
 const port = process.env.PORT || 8081;
 
-// TODO: set up websocket to listen for events..
+// set up json middleware
+app.use(express.json()); // for parsing application/json
 
 // first access is to the api endpoints
 app.use('/api', router);
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
 // everything else gets served the react app
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname+'/../client/dist/index.html'));
 });
+
+// TODO: set up websocket to listen for events..
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
